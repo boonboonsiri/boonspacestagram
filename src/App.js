d7be173403748151bda149862ca0591e6cd1420d
boon21 <90270074+boon21@users.noreply.github.com>
2022-01-12 22:22:33 -0500

    First commit. Functional app, don't call api yet

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import Canvas from './components/canvas.js';
+import {useRef, useCallback} from 'react'
+import searchNasa from './components/searchNasa.js'
 
 function App() {
+  // API key: kQv670YToeq0SuZ39J2d1V9rMjqZkVsjwNeLkYHt
+
+  //const { canvases } = searchNasa();
+
+
+  var passObj = {
+    img: "https://images.unsplash.com/photo-1641966961473-5b083fd62fff?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2MXx8fGVufDB8fHx8&auto=format&fit=crop&w=800&q=60",
+    title: "SATURN",
+    date: "January 21, 2001",
+    data: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas lobortis, est ut posuere auctor, lacus nisi mollis sapien, vel ullamcorper purus ex eu enim. Etiam quis lorem consequat"
+
+  }
+
+  let canvases =[passObj,passObj,passObj,passObj,passObj,passObj,passObj,passObj,passObj,passObj]
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="titleHeader">
+        <h1 className>Spacestagram</h1>
+      </span>
+      <span>
+        <Canvas obj={passObj}/>
+        <Canvas obj={passObj}/>
+
+        {canvases.map((passObj, i) => {
+
+          return <Canvas obj={passObj}/>;
+      })}
+
+      </span>
     </div>
   );
 }
